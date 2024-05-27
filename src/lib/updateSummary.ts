ee2d9c858cd43f2dd4bb985c0b0bee4df96f68c4
Nathan Arthur <nathan@nathanarthur.com>
2024-05-27 10:03:17 -0400

    update summary format

--- src/lib/updateSummary.ts
+++ src/lib/updateSummary.ts
@@ -1,17 +1,56 @@
 import { Issue } from "./scanUrls.js";
 import core from "@actions/core";
 
+function addCodeTable(issues: Issue[]) {
+  const codes = Array.from(new Set(issues.map((issue) => issue.code)));
+  const headerRow = [
+    "Code",
+    "Description",
+    "Type",
+    "Runner",
+    "Instance Count",
+    "URL Count",
+    "Selector Count",
+  ];
+  const rows = codes.map((code) => {
+    const instances = issues.filter((issue) => issue.code === code);
+    const urlCount = Array.from(
+      new Set(instances.map((issue) => issue.url))
+    ).length;
+    const selectorCount = Array.from(
+      new Set(instances.map((issue) => issue.selector))
+    ).length;
+    return [
+      code,
+      instances[0].message,
+      instances[0].type,
+      instances[0].runner || "",
+      instances.length.toString(),
+      urlCount.toString(),
+      selectorCount.toString(),
+    ];
+  });
+  core.summary.addTable([headerRow, ...rows]);
+}
+
+function addInstanceTable(issues: Issue[]) {
+  const headerRow = ["URL", "Code", "Selector", "Context"];
+  const rows = issues.map((issue) => [
+    issue.url,
+    issue.code,
+    issue.selector,
+    issue.context,
+  ]);
+  core.summary.addTable([headerRow, ...rows]);
+}
+
 export default async function updateSummary(issues: Issue[]) {
   if (!issues.length) {
     return;
   }
-  const headerRow = Object.keys(issues[0]).map((k) => ({
-    data: k,
-    header: true,
-  }));
-  const rows = issues.map((issue) =>
-    Object.values(issue).map((v) => v.toString())
-  );
-  core.summary.addTable([headerRow, ...rows]);
+
+  addCodeTable(issues);
+  addInstanceTable(issues);
+
   core.summary.write({ overwrite: true });
 }

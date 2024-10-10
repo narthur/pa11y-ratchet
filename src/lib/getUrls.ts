45e59186a81e836a486c1bdef46aad03e3957599
lcflight <lcarthur747@gmail.com>
2024-10-10 15:08:36 -0400

    adds better logging if parsing fails

--- src/lib/getUrls.ts
+++ src/lib/getUrls.ts
@@ -12,7 +12,21 @@ type Response = {
 export default async function getUrls(sitemapUrl: string): Promise<string[]> {
   const siteMap = await fetch(sitemapUrl).then((res) => res.text());
 
-  return xml2js
-    .parseStringPromise(siteMap)
-    .then((res: Response) => res.urlset.url.map((url) => url.loc[0]));
+  try {
+    const parsed = await xml2js.parseStringPromise(siteMap);
+    return (parsed as Response).urlset.url.map((url) => url.loc[0]);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(
+        "Failed to parse sitemap:\nError Message: " +
+          error.message +
+          "\nSitemap URL: " +
+          sitemapUrl
+      );
+    } else {
+      throw new Error(
+        "Failed to parse sitemap:\nUnknown error\nSitemap URL: " + sitemapUrl
+      );
+    }
+  }
 }

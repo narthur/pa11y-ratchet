6c8db6ec59c16e63598773e6ce08ec44ef2bc880
Nathan Arthur <nathan@nathanarthur.com>
2025-03-06 09:56:50 -0500

    use --no-sandbox

--- src/lib/scanUrls.spec.ts
+++ src/lib/scanUrls.spec.ts
@@ -1,6 +1,10 @@
 import { describe, it, expect, vi } from "vitest";
 import scanUrls from "./scanUrls.js";
 import pa11y from "pa11y";
+import * as fs from "node:fs/promises";
+import getInputs from "./getInputs.js";
+
+vi.mock("node:fs/promises");
 
 describe("scanUrls", () => {
   it("inclueds exceptions in returned issues", async () => {
@@ -14,5 +18,43 @@ describe("scanUrls", () => {
         message: "the_error_message",
       }),
     ]);
+  });
+
+  it("uses noSandbox arg", async () => {
+    await scanUrls(["https://example.com"]);
+
+    expect(pa11y).toHaveBeenCalledWith(
+      "https://example.com",
+      expect.objectContaining({
+        chromeLaunchConfig: expect.objectContaining({
+          args: ["--no-sandbox"],
+        }),
+      })
+    );
+  });
+
+  it("preserves provided chromeLaunchConfig", async () => {
+    vi.mocked(getInputs).mockReturnValue({
+      configPath: "the_config_path",
+    } as any);
+
+    vi.mocked(fs.readFile).mockResolvedValue(
+      JSON.stringify({
+        chromeLaunchConfig: {
+          anotherOption: "the_option_value",
+        },
+      }) as any
+    );
+
+    await scanUrls(["https://example.com"]);
+
+    expect(pa11y).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.objectContaining({
+        chromeLaunchConfig: expect.objectContaining({
+          anotherOption: "the_option_value",
+        }),
+      })
+    );
   });
 });
--- src/lib/scanUrls.ts
+++ src/lib/scanUrls.ts
@@ -14,22 +14,42 @@ export type Issue = {
   runner?: string;
 };
 
+type RawPa11yOptions = NonNullable<Parameters<typeof pa11y>[1]>;
+
+type LaunchConfig = Partial<
+  NonNullable<RawPa11yOptions["chromeLaunchConfig"]>
+> & {
+  args?: string[];
+};
+
+type Pa11yOptions = Omit<RawPa11yOptions, "chromeLaunchConfig"> & {
+  chromeLaunchConfig?: LaunchConfig;
+};
+
 export default async function scanUrls(urls: string[]): Promise<Issue[]> {
   const issues = [];
   const len = urls.length;
   const { configPath } = getInputs();
   const absConfigPath = path.resolve(configPath);
 
-  let pa11yOpts = {};
+  let pa11yOpts: Pa11yOptions = {};
   if (configPath !== "") {
     const configJSON = await fs.readFile(absConfigPath, "utf8");
     pa11yOpts = JSON.parse(configJSON);
   }
 
+  pa11yOpts = {
+    ...pa11yOpts,
+    chromeLaunchConfig: {
+      args: ["--no-sandbox"],
+      ...pa11yOpts?.chromeLaunchConfig,
+    },
+  };
+
   for (const [i, url] of urls.entries()) {
     const key = `${i + 1}/${len}: ${url}`;
     console.time(key);
-    const res = await pa11y(url, pa11yOpts).catch((err) => {
+    const res = await pa11y(url, pa11yOpts as RawPa11yOptions).catch((err) => {
       console.error(`Error scanning URL: ${url}`, err);
       return {
         issues: [
